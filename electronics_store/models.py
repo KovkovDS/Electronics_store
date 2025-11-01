15119d3989328522212af4ded8d2dfbf4e98551d
KovkovDS <dmitrii.s.kovkov@outlook.com>
2025-11-01 18:33:41 +0300

    creating models for users and electronics_store applications

--- electronics_store/models.py
+++ electronics_store/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from mptt.models import MPTTModel
 
-# Create your models here.
+
+class VendorType(models.Choices):
+    FACTORY = 'Завод'
+    IE = 'ИП'
+    RN = 'Розничная сеть'
+
+
+class Vendor(models.Model):
+    """
+    Модель поставщика
+    """
+    name = models.CharField(unique=True, max_length=200, verbose_name="Наименование", db_index=True)
+    type = models.CharField(choices=VendorType.choices, default=VendorType.FACTORY, verbose_name='Тип')
+    contacts = models.ForeignKey('Contacts', on_delete=models.CASCADE, verbose_name='Контакты')
+    email = models.EmailField(blank=True, verbose_name="E-mail")
+    products = models.ManyToManyField('Product', verbose_name='Товар', db_index=True)
+    arrears = models.FloatField(null=True, blank=True, verbose_name="Задолженность перед поставщиком")
+    release_date = models.DateTimeField(auto_now_add=True, verbose_name="Время создания", db_index=True)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True, related_name='children',
+                               db_index=True, verbose_name='Поставщик')
+    supplier_level = models.IntegerField(blank=True, verbose_name="Уровень")
+
+    def __str__(self):
+        return f'{self.type} {self.name} {self.email}'
+
+    class Meta:
+        verbose_name = 'Поставщик'
+        verbose_name_plural = 'Поставщики'
+        ordering = ["parent", "name", "type", "email"]
+
+
+class Contacts(models.Model):
+    country = models.CharField(max_length=200, verbose_name="Страна")
+    city = models.CharField(max_length=200, verbose_name="Город")
+    street = models.CharField(max_length=200, verbose_name="Улица")
+    house = models.CharField(max_length=200, verbose_name="Дом")
+
+    def __str__(self):
+        return f"{self.country}, {self.city}, {self.street}. {self.house}"
+
+    class Meta:
+        verbose_name = 'Контакты'
+        verbose_name_plural = 'Контакты'
+        ordering = ["country", "city", "street", "house"]
+
+
+class Product(models.Model):
+    name = models.CharField(unique=True, max_length=200, verbose_name="Наименование", db_index=True)
+    model = models.CharField(unique=True, max_length=200, verbose_name="Модель", db_index=True)
+    release_date = models.DateField(auto_now_add=True, verbose_name="Дата выхода продукта", db_index=True)
+
+    def __str__(self):
+        return f"{self.name} {self.model}"
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ["model", "name"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    """Класс менеджера для создания объектов модели "Пользователь"."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Метод создания объекта "пользователь" модели "Пользователь"."""
+
+        if not email:
+            raise ValueError("Адрес электронной почты должен быть указан")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Метод создания объекта "суперпользователь" модели "Пользователь"."""
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Суперпользователь должен иметь is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """Класс модели "Пользователь"."""
+
+    email = models.EmailField(unique=True, verbose_name="Адрес электронной почты")
+    phone_number = models.CharField(max_length=11, blank=True, null=True, verbose_name="Номер телефона")
+    city = models.CharField(max_length=100, blank=True, verbose_name="Город")
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['email', 'phone_number']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        """Метод для описания человеко читаемого вида модели "Пользователь"."""
+
+        return self.email
+
+    class Meta:
+        """Класс для изменения поведения полей модели "Пользователь"."""
+
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email", "phone_number"]
